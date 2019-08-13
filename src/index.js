106da06ae88579644e3abb7f9c9cb43226082f9d
TrueMistake <ooghon@gmail.com>
2019-08-13 12:07:34 +0300

    git init

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/app';
+
+ReactDOM.render(<App/>,document.getElementById('root'));
+
+
+
+
+
+
+
+
+
+
+
+
+
